09be929516249e1edc0c6f1d68acc0f45403459f
Nguyen1826 <singuyenrobin@gmail.com>
2025-05-10 14:56:54 +0700

    Fix path

--- label2coco.py
+++ label2coco.py
@@ -1,143 +1,141 @@
+import glob
+import imagesize
 import os
-from pathlib import Path
-
-import fire
+import json
 from PIL import Image
-from sahi.utils.coco import Coco, CocoAnnotation, CocoCategory, CocoImage
-from sahi.utils.file import save_json
 from tqdm import tqdm
+import argparse
 
-CATEGORY_ID_TO_NAME = {
-    "0": "ignore",
-    "1": "pedestrian",
-    "2": "people",
-    "3": "bicycle",
-    "4": "car",
-    "5": "van",
-    "6": "truck",
-    "7": "tricycle",
-    "8": "awning-tricycle",
-    "9": "bus",
-    "10": "motor",
-    "11": "others",
-}
+def convert(dir_data):
 
-CATEGORY_ID_REMAPPING = {
-    "1": "0",
-    "2": "1",
-    "3": "2",
-    "4": "3",
-    "5": "4",
-    "6": "5",
-    "7": "6",
-    "8": "7",
-    "9": "8",
-    "10": "9",
-}
+  train_data = dir_data + '/VisDrone2019-DET-train/'
+  val_data = dir_data + '/VisDrone2019-DET-val/'
+  test_data = dir_data + '/VisDrone2019-DET-test-dev/'
+  loops = [train_data, val_data, test_data]
+  for l in loops:
+      print('Solving ', l)
+      dict_coco = {}
 
-NAME_TO_COCO_CATEGORY = {
-    "pedestrian": {"name": "pedestrian", "supercategory": "person"},
-    "people": {"name": "people", "supercategory": "person"},
-    "bicycle": {"name": "bicycle", "supercategory": "bicycle"},
-    "car": {"name": "car", "supercategory": "car"},
-    "van": {"name": "van", "supercategory": "truck"},
-    "truck": {"name": "truck", "supercategory": "truck"},
-    "tricycle": {"name": "tricycle", "supercategory": "motor"},
-    "awning-tricycle": {"name": "awning-tricycle", "supercategory": "motor"},
-    "bus": {"name": "bus", "supercategory": "bus"},
-    "motor": {"name": "motor", "supercategory": "motor"},
-}
+      dir_imgs = './images/'
 
+      ''' Key: images '''
+      print('Solving images')
+      dict_image_and_id = {}
+      dict_coco['images'] = []
+      img_id = 0
+      for img in tqdm(glob.glob(l + dir_imgs + '*')):
+          # image = Image.open(img)
+          width, height = imagesize.get(img)
+          # file_name = os.path.split(img)
+          file_name_save = os.path.split(img)[-1]
+          dict_coco['images'].append({
+              "id" : img_id,
+              "license" : 1,
+              "height" : height,
+              "width" : width,
+              "file_name": file_name_save
+          })
+          dict_image_and_id[file_name_save] = img_id
+          img_id = img_id + 1
 
-def visdrone_to_coco(
-    data_folder_dir,
-    output_file_path,
-    category_id_remapping=None,
-):
-    """
-    Converts visdrone-det annotations into coco annotation.
+      ''' Key: annotations '''
+      print('Solving annotations')
+      dir_labels = '/annotations/'
+      dict_coco['annotations'] = []
+      anno_id = 0
+      for file_txt in tqdm(glob.glob(l + dir_labels + '*.txt')):
+          # with open(file_txt, 'r') as f:
+          #     annotations = f.read()
+          annotations = open(file_txt,'r').read()
+          annotations = annotations.split('\n')
+          for i in range(0, len(annotations)):
+              annotations[i] = annotations[i].split(',')
+          
+          annotations = annotations[:-1]
+          for detection in annotations:
+              category_id = int(detection[5])
+              bbox = [int(detection[0]), int(detection[1]), int(detection[2]), int(detection[3])]
+              area = int(detection[2]) * int(detection[3])
+              segmentation = []
+              iscrowd = 0
+              img_name = os.path.splitext(os.path.split(file_txt)[-1])[0] + '.jpg'
+              image_id = dict_image_and_id[img_name]
 
-    Args:
-        data_folder_dir: str
-            'VisDrone2019-DET-train' folder directory
-        output_file_path: str
-            Output file path
-        category_id_remapping: dict
-            Used for selecting desired category ids and mapping them.
-            If not provided, VisDrone2019-DET mapping will be used.
-            format: str(id) to str(id)
-    """
+              dict_coco['annotations'].append({
+              "id": anno_id,
+                  "image_id": image_id,
+                  "category_id": category_id,
+                  "bbox": bbox,
+                  "area": area,
+                  "iscrowd": 0,
+                  "ignore": 0
+              })
 
-    # init paths/folders
-    input_image_folder = str(Path(data_folder_dir) / "images")
-    input_ann_folder = str(Path(data_folder_dir) / "annotations")
+              anno_id = anno_id + 1
 
-    image_filepath_list = os.listdir(input_image_folder)
+      ''' Key: categories '''
 
-    Path(output_file_path).parents[0].mkdir(parents=True, exist_ok=True)
+      '''
+      pedestrian (1), people (2), bicycle (3), car (4), van (5), 
+      truck (6), tricycle (7), awning-tricycle (8), bus (9), motor (10), others (11)
+      '''
+      dict_coco['categories'] = [{
+          "id": 1,
+          "name": "pedestrian",
+          "supercategory": "none"},
+          {
+          "id": 2,
+          "name": "people",
+          "supercategory": "none"},
+          {
+          "id": 3,
+          "name": "bicycle",
+          "supercategory": "none"},
+          {
+          "id": 4,
+          "name": "car",
+          "supercategory": "none"},
+          {
+          "id": 5,
+          "name": "van",
+          "supercategory": "none"},
+          {
+          "id": 6,
+          "name": "truck",
+          "supercategory": "none"},
+          {
+          "id": 7,
+          "name": "tricycle",
+          "supercategory": "none"},
+          {
+          "id": 8,
+          "name": "awning-tricycle",
+          "supercategory": "none"},
+          {
+          "id": 9,
+          "name": "bus",
+          "supercategory": "none"},
+          {
+          "id": 10,
+          "name": "motor",
+          "supercategory": "none"},
+          {
+          "id": 11,
+          "name": "others",
+          "supercategory": "none"}
+          ]
 
-    if category_id_remapping is None:
-        category_id_remapping = CATEGORY_ID_REMAPPING
+      with open('annotations_VisDrone_' + l.split('-')[-1][:-1] + '.json', 'w') as f:
+          json.dump(dict_coco, f)
 
-    # init coco object
-    coco = Coco()
-    # append categories
-    for category_id, category_name in CATEGORY_ID_TO_NAME.items():
-        if category_id in category_id_remapping.keys():
-            remapped_category_id = category_id_remapping[category_id]
-            coco_category = NAME_TO_COCO_CATEGORY[category_name]
-            coco.add_category(
-                CocoCategory(
-                    id=int(remapped_category_id),
-                    name=coco_category["name"],
-                    supercategory=coco_category["supercategory"],
-                )
-            )
+def get_args():
+    parser = argparse.ArgumentParser('Train')
+    parser.add_argument('--data_dir', type=str, default='./',
+                        help='Data dir', dest='data_dir')
+    args = parser.parse_args()
 
-    # convert visdrone annotations to coco
-    for image_filename in tqdm(image_filepath_list):
-        # get image properties
-        image_filepath = str(Path(input_image_folder) / image_filename)
-        annotation_filename = image_filename.split(".jpg")[0] + ".txt"
-        annotation_filepath = str(Path(input_ann_folder) / annotation_filename)
-        image = Image.open(image_filepath)
-        cocoimage_filename = str(Path(image_filepath)).split(str(Path(data_folder_dir)))[1]
-        if cocoimage_filename[0] == os.sep:
-            cocoimage_filename = cocoimage_filename[1:]
-        # create coco image object
-        coco_image = CocoImage(file_name=cocoimage_filename, height=image.size[1], width=image.size[0])
-        # parse annotation file
-        file = open(annotation_filepath, "r")
-        lines = file.readlines()
-        for line in lines:
-            # parse annotation bboxes
-            new_line = line.strip("\n").split(",")
-            bbox = [
-                int(new_line[0]),
-                int(new_line[1]),
-                int(new_line[2]),
-                int(new_line[3]),
-            ]
-            # parse category id and name
-            category_id = new_line[5]
-            if category_id in category_id_remapping.keys():
-                category_name = CATEGORY_ID_TO_NAME[category_id]
-                remapped_category_id = category_id_remapping[category_id]
-            else:
-                continue
-            # create coco annotation and append it to coco image
-            coco_annotation = CocoAnnotation.from_coco_bbox(
-                bbox=bbox,
-                category_id=int(remapped_category_id),
-                category_name=category_name,
-            )
-            if coco_annotation.area > 0:
-                coco_image.add_annotation(coco_annotation)
-        coco.add_image(coco_image)
+    return args
 
-    save_path = output_file_path
-    save_json(data=coco.json, save_path=save_path)
-
-
-if __name__ == "__main__":
-    fire.Fire(visdrone_to_coco)
+if __name__ == '__main__':
+    args = get_args()
+    convert(args.data_dir)
